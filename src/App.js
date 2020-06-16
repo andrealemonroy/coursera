906604072e22075ebc3ae2bf222cc655f1b83a49
AndreaMonroy <andrea@kambista.com>
2020-06-16 11:21:27 -0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
